ac12eec8b43305368568c2827e31719bdab65fb5
Matt Arnmor <74899348+mwaBarton@users.noreply.github.com>
2023-01-24 13:33:51 +0000

    Added hello world

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,8 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
-
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
